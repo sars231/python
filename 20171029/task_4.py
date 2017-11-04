9bb5ad2126620ae4e4194e4a559dc7181797a2bf
宾宇波 <328467238@qq.com>
2017-11-04 21:30:37 +0800

    换一种方式

--- 20171029/task_4.py
+++ 20171029/task_4.py
@@ -5,6 +5,9 @@ task 4
 
 
 for i in range(8):
-    for j in range(4):
-        print "* +",
+    for j in range(8):
+        if j%2==0:
+            print "+",
+        else:
+            print "*",
     print
