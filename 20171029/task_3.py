6eccd4e986f9a108467a707a61f5c35c8c313dd7
宾宇波 <328467238@qq.com>
2017-11-04 21:27:09 +0800

    精简过程

--- 20171029/task_3.py
+++ 20171029/task_3.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
-"""
-task 3
-"""
+
+
 
 num=input("please input a sanweishu number:")
 a=num/100
-b=(num-a*100)/10
-c=num-a*100-b*10
+b=num%100/10
+c=num%10
 print "baiweishu is "+str(a)
 print "shiweishu is "+str(b)
 print "geweishu is "+str(c)
